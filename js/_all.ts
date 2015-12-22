3601e1f7cd8e751cf34805f8271e6fd471860004
daxnet <daxnet@outlook.com>
2015-12-22 13:51:57 +0800

    Initial commit of the source code.

---
+++ js/_all.ts
@@ -0,0 +1,5 @@
+/// <reference path="typings/angularjs/angular.d.ts" />
+/// <reference path="models/staff.ts" />
+/// <reference path="controllers/WhosNextController.ts" />
+/// <reference path="interfaces/IWhosNextScope.ts" />
+/// <reference path="application.ts" />
---
+++ js/application.ts
@@ -0,0 +1,8 @@
+/// <reference path="_all.ts" />
+
+module WhosNextApplication {
+    'use strict';
+    
+    angular.module('whosnext', [])
+           .controller('whosNextController', WhosNextController);
+}
